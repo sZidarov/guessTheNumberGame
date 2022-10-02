d740f302682548859ed8c64cac0cb89a14c81bc0
sZidarov <s.zidarov@gmail.com>
2022-10-02 15:02:06 +0300

    added difficulty to the game. Only 10 tries

--- guessTheNumberGame.js
+++ guessTheNumberGame.js
@@ -5,25 +5,37 @@ const readline = require('readline').createInterface({
 });
 let computerGuess = Math.floor(Math.random() * 100);
 let guess;
+let tryCount = 10;
 
 let recursiveAsyncReadLine = function() {
-    readline.question('Guess the number (0-100): ', number => {
+    readline.question(`Guess the number from 0 to 100, you have ${tryCount} more tries: ` , number => {
        guess = Number(number);
+ 
        if (guess <= 100 && guess>=0){
         if (guess == computerGuess){
-            console.log('You guess it!');
+            console.log('You guessed it on the ' + (11-tryCount) + ' try!');
             return readline.close();
         }else if (guess < computerGuess){
             console.log('Too Low!');
+            tryCount--;
+            //console.log(`You have ${tryCount} tries left!`);
             recursiveAsyncReadLine();
         }else if (guess > computerGuess){
             console.log('Too High!');
+            tryCount--;
+            //console.log(`You have ${tryCount} tries left!`);
             recursiveAsyncReadLine();
         }
-    }else {
+        }else {
         console.log('Invalid input! Try again...');
+        tryCount--;
         recursiveAsyncReadLine();
-    }
+        }
+        if (tryCount==0){
+            console.log('Aborting...');
+            return readline.close();
+        }
+        
     });
 }
 recursiveAsyncReadLine();
